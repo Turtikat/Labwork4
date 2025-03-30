31a2106bc2b58bce9ce0f9f2e06bf835de3de80a
Turtikat <katja.turtiainen@student.laurea.fi>
2025-03-30 14:55:40 +0300

    new LabLoginApp

---
+++ src/enviroments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCuyIkyorlLjekWs5CDVQvTiDtY70ImPWw",
+      authDomain: "labwork4kt.firebaseapp.com",
+      projectId: "labwork4kt",
+      storageBucket: "labwork4kt.appspot.com",
+      messagingSenderId: "448536460288",
+      appId: "1:448536460288:web:14c7a5f39a49f61266ace5",
+      measurementId: "G-TBMEJX9HFW"
+    }
+  };
+  
